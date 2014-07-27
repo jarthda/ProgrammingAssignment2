1828aa5b7a3eb6be27e105aa9fdc664c270833a4
Jarthda <john.ligda@gmail.com>
2014-07-27 19:37:32 +0100

    Ad Comments
    Comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,25 +1,8 @@
- ## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-	m <- NULL
-	set < - function(y) {
-		x << - y
-		m << - NULL
-	}
-	get <- function() x
-	setsolve <- function(solve) m <<- mean
-	getsolve <- function() m
-	list(set = set, get = get,
-		setsolve = setsolve,
-		getsolve = getsolve)	
-
-}
+##  cache potentially time-consuming computation of solving for inverse of a matix
+## 
 
 
-## Write a short comment describing this function
+
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
@@ -31,4 +14,19 @@ cacheSolve <- function(x, ...) {
         data <- x$get()
         m <- solve(data, ...)
         x$setsolve(m)	
+}
+
+makeCacheMatrix <- function(x = matrix()) {
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setsolve <- function(solve) m <<- solve
+  getsolve <- function() m
+  list(set = set, get = get,
+       setsolve = setsolve,
+       getsolve = getsolve)  
+  
 }
